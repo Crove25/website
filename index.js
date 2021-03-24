b36a1622f86ce35a6c3c42a880178578bc04fde2
Franco <71045051+Crove25@users.noreply.github.com>
2021-03-24 14:50:15 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function GoFacebook(){
+    window.open('https://www.facebook.com/profile.php?id=100041110690847');
+}
+
+function GoInstagram(){
+    window.open('https://www.instagram.com/jafs_viajes_logistica/?hl=es-la');
+}
